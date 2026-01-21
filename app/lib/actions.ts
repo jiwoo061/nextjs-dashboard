847e26bd57c726d430d19b5dd10799fb0f30277d
jiwoo <jiwoo061@gmail.com>
2026-01-21 14:04:49 +0900

    ch14: Adding Authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@
  import postgres from 'postgres';
  import { revalidatePath } from 'next/cache';
  import { redirect } from 'next/navigation';
+ import { signIn } from '@/auth';
+ import { AuthError } from 'next-auth';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -22,6 +24,25 @@ const FormSchema = z.object({
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 export async function updateInvoice(
   id: string, 
